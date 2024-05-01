449761c2887998398f4150054dbc085b899d94cd
nicoleami <128334492+nicoleami@users.noreply.github.com>
2024-05-01 20:21:41 -0300

    Add files via upload

---
+++ programacao/src/main/java/com/example/demo/Cidade.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+public class Cidade {
+    private Long id;
+    private String nome;
+    private String estado;
+    private String pais;
+    private String populacao;
+
+    public Cidade(Long id, String nome, String estado, String pais, String populacao) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+        this.pais = pais;
+        this.populacao = populacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getPopulacao() {
+        return populacao;
+    }
+
+    public void setPopulacao(String populacao) {
+        this.populacao = populacao;
+    }
+}
---
+++ programacao/src/main/java/com/example/demo/CidadeController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+public class CidadeController {
+    private List<Cidade> cidades = new ArrayList<>();
+
+
+    @GetMapping
+    public List<Cidade> mostraCidades(){
+        return cidades;
+    }
+
+    @PostMapping
+    public List<Cidade> cadastraCidade(@RequestBody Cidade cidade){
+        cidades.add(cidade);
+        return cidades;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarDisciplina(@PathVariable Long id) {
+        cidades.removeIf(cidade -> cidade.getId().equals(id));
+    }
+
+    @PutMapping("/{id}")
+    public List<Cidade> atualizaCidade(@PathVariable Long id, @RequestBody Cidade cidadeAtualizada) {
+        for (Cidade cidade : cidades) {
+            if (cidade.getId().equals(id)) {
+                cidade.setNome(cidadeAtualizada.getNome());
+                cidade.setEstado(cidadeAtualizada.getEstado());
+                cidade.setPais(cidadeAtualizada.getPais());
+                cidade.setPopulacao(cidadeAtualizada.getPopulacao());
+                break;
+            }
+        }
+        return cidades;
+    }
+
+}
